b9a734bbd7bc1c6726960a9e3b0b48c225415800
Newton Gain <91782099+NewtonGain@users.noreply.github.com>
2024-03-31 18:09:41 +0600

    hi

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const hostname = '0.0.0.0';
+const port = 3000;
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`)});
